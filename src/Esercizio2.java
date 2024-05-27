72ad237aed45e5fd15ce0fd9bae92656012b94e0
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-27 14:24:18 +0200

    Aggiunta dei logback e inizio esercizio 2

--- src/Esercizio2.java
+++ src/Esercizio2.java
@@ -1,2 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Esercizio2 {
+    public class arrayUtilizzato{
+        private static final Logger logger =LoggerFactory.getLogger(arrayUtilizzato.class);
+
+        public static void main(String[] args) {
+
+        }
+    }
 }
