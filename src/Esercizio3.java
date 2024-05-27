5cb01cf227c1aa8e0eae9566b3995f717d505d2d
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-27 14:56:44 +0200

    Prima parte del Esercizio 3 possibile impostazione

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
+
+
 public class Esercizio3 {
+    private static final Logger logger= LoggerFactory.getLogger(Esercizio3.class);
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+         try {
+             System.out.println("Inserisci il numero di Kilometri che hai percorso: ");
+             double kilometriPercorsi= scanner.nextDouble();
+
+             System.out.println("Inserisci il numero di litri di carburante che hai usato:");
+             double carburanteUsato= scanner.nextDouble();
+         }
+    }
 }
